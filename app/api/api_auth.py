bd20254f6b773c2244e15b9bc45e9508fbb003cf
lijianan <lijianan@tezign.com>
2023-10-29 17:59:06 +0800

    feat: secret_key提取到环境变量

--- app/api/api_auth.py
+++ app/api/api_auth.py
@@ -8,9 +8,9 @@ import os
 
 router = APIRouter()
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOW_USERNAME = os.environ.get("USERNAME", "admin")
 ALLOW_PASSWORD = os.environ.get("PASSWORD", "admin")
 
